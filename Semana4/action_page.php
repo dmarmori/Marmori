359890d4d18ee3ed54cb807d282396caa311a32f
dmarmori <54649635+dmarmori@users.noreply.github.com>
2019-09-04 21:21:25 -0300

    Add files via upload

--- Semana4/action_page.php
+++ Semana4/action_page.php
@@ -8,10 +8,28 @@ $apellido = $_GET['apellido'];
 
 echo 'Su nombre es ',$nombre,' ',$apellido,'<br>';
 
-echo 'Su nombre es '.$_GET['nombre'];
+echo 'Su nombre es '.$_GET['nombre'] . ' ' .$_GET['apellido'];//paraetros por GET
+
+echo '<br>';
+echo '<br>';
+
+//$archivo = fopen('archivo.txt', 'a');
+//fwrite($archivo, $_GET["nombre"]."\n");
+//fwrite($archivo, $_GET["apellido"]."\n");
+//fclose($archivo);
+
+$MiObjeto = New stdClass();
+$MiObjeto->nombre=$_GET['nombre'];
+$MiObjeto->apellido=$_GET['apellido'];
+
+
+var_dump(json_encode($MiObjeto));
 
 $archivo = fopen('archivo.txt', 'a');
-fwrite($archivo, $_GET["nombre"]."\n");
-fwrite($archivo, $_GET["apellido"]."\n");
+
+fwrite($archivo,json_encode($MiObjeto));
+fclose($archivo);
+
+
 
 ?>
